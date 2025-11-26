51e44fb12a5670e91365a43b00456fab30bfa017
pawelekbyra <pawelek.byra@gmail.com>
2025-11-26 22:50:21 +0100

    Dodanie podstrony /dupa

---
+++ app/dupa/page.tsx
@@ -0,0 +1,8 @@
+export default function DupaPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8">
+      <h1 className="text-4xl font-bold mb-4">Dupa</h1>
+      <p className="text-lg">Zgodnie z życzeniem, oto nowa podstrona.</p>
+    </div>
+  );
+}
